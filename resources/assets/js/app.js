58d8a3857f60337cd9d354ba4dc8504edb96d91b
Filip Lekic <lambdaconda@gmail.com>
2018-02-28 15:39:12 +0100

    Improved 'Like a Boss' highlighting TR-iMuiKicJ

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,13 @@
 import Disqus from "./components/Disqus";
 
-// add syntax highlighting like a boss, need for speed (2018)
-const Prism = require('prismjs');
+// add syntax highlighting like a boss, Need For Speed (2018)
+import Prism from 'prismjs';
+require('prismjs/components/prism-bash.min');
+require('prismjs/components/prism-php.min');
+require('prismjs/components/prism-php-extras.min');
+require('prismjs/components/prism-javascript.min');
+require('prismjs/plugins/toolbar/prism-toolbar.min');
+require('prismjs/plugins/show-language/prism-show-language.min');
 
 const app = new Vue({
     components: {
