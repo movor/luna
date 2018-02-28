4d30e5ab391d50be4042a2f78c6b70195d6318e2
Filip Lekic <lambdaconda@gmail.com>
2018-02-28 14:43:59 +0100

    Better seed template, add code syntax highlighting TR-KBgGfSnK

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,5 +1,8 @@
 import Disqus from "./components/Disqus";
 
+// add syntax highlighting like a boss, need for speed (2018)
+const Prism = require('prismjs');
+
 const app = new Vue({
     components: {
         'appDisqus': Disqus
