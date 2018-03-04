720fe758971b6ebdad202c077f2709898d6ee5e3
Vlada Kovic <vlada.kovic@gmail.com>
2018-03-04 13:05:41 +0100

    Admin new user creation TR-qTzzX1eW

--- app/Http/Controllers/Admin/UserCrudController.php
+++ app/Http/Controllers/Admin/UserCrudController.php
@@ -2,7 +2,7 @@
 
 use App\Models\User;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
-use Request;
+use Illuminate\Http\Request;
 
 class UserCrudController extends CrudController
 {
@@ -31,16 +31,58 @@ class UserCrudController extends CrudController
             ->addField([
                 'label' => 'Email',
                 'name' => 'email',
+            ])
+            ->addField([
+                'name' => 'password',
+                'label' => 'Password',
+                'type' => 'password'
+            ])->addField([
+                'name' => 'password_confirmation',
+                'label' => 'Confirm Password',
+                'type' => 'password'
             ]);
     }
 
     public function store(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $this->handlePassword($request);
+
         return parent::storeCrud();
     }
 
     public function update(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            // TODO.SOLVE
+            // Unique email is omitted to allow user to update pass only.
+            // There is a problem in case user changes email to already existing one.
+            'password' => 'sometimes|string|min:6|confirmed'
+        ]);
+
+        $this->handlePassword($request);
+
         return parent::updateCrud();
+    }
+
+    protected function handlePassword(Request $request)
+    {
+        $request->merge(['password' => bcrypt($request->password)]);
+    }
+
+    protected function validateFields(Request $request, $allowEmptyPassword = false)
+    {
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => $allowEmptyPassword ? '' : 'required|string|min:6|confirmed',
+        ]);
     }
 }
