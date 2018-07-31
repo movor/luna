9d33b3850f4ee77788b7a22936f3a68301f93cb8
Vladimir Ković <vlada.kovic@gmail.com>
2018-07-31 19:52:07 +0200

    Minor improvements for existing movor packages TR-c5Rhqs9Z

---
+++ packages/movor/laravel-meta/src/package/Models/Meta.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace Movor\LaravelMeta\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Meta extends Model
+{
+    protected $guarded = [];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saving(function (Meta $model) {
+            if (is_null($model->realm)) {
+                $model->realm = config('laravel-meta.default_realm');
+            }
+        });
+    }
+
+    /**
+     * Set table name dynamically
+     *
+     * @return string
+     */
+    public function getTable()
+    {
+        return config('laravel-meta.table_name');
+    }
+
+    /**
+     * Metable relation
+     *
+     * @return MorphTo
+     */
+    public function metable()
+    {
+        return $this->morphTo();
+    }
+
+    /**
+     * Filter meta by standard values (realm, type and id),
+     * and order by meta key
+     *
+     * @param        $query
+     * @param null   $realm
+     * @param string $metableType
+     * @param string $metableId
+     *
+     * @return mixed
+     */
+    public function scopeFilter($query, $realm, $metableType, $metableId)
+    {
+        return $query->where([
+            'realm' => !is_null($realm) ? $realm : config('laravel-meta.default_realm'),
+            'metable_type' => $metableType,
+            'metable_id' => $metableId,
+        ])->orderBy('key');
+    }
+
+    /**
+     * Setter for "value" field
+     *
+     * @param $value
+     */
+    public function setValueAttribute($value)
+    {
+        $this->attributes['value'] = is_array($value)
+            ? json_encode($value)
+            : $value;
+    }
+
+    /**
+     * Getter for "value" field
+     *
+     * @param $value
+     *
+     * @return array|string
+     */
+    public function getValueAttribute($value)
+    {
+        // Check if $value is json and if it is, return array,
+        // otherwise return $value
+
+        $array = json_decode($value, true);
+
+        return json_last_error() === JSON_ERROR_NONE
+            ? $array
+            : $value;
+    }
+}
