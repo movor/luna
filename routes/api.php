196622e82892f0d47b279612e540d971cffdff43
Vladimir Ković <vlada.kovic@gmail.com>
2018-08-10 14:18:24 +0200

    Remove API routes TR-kaBw226o

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,3 @@
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-// Authentication
-Route::post('/user/register', 'Api\UserController@register');
-Route::get('/user/current', 'Api\UserController@current')->middleware('auth:api');
-
-// Blog
-Route::get('/blog-post', 'Api\BlogPostController@index');
-Route::get('/blog-post/{id}', 'Api\BlogPostController@show');
