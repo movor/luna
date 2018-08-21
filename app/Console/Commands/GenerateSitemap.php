1949e9a0f652fb71893b5c88391a548d9ed507d5
Vladimir Ković <vlada.kovic@gmail.com>
2018-08-21 15:57:20 +0200

    Sitemap generator TR-ujczKNBi

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\SitemapGenerator;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate sitemap xml file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        SitemapGenerator::create(env('APP_URL'))
+            ->hasCrawled(function (Url $url) {
+                // Exclude all urls with query params
+                return strpos($url->url, '?') === false
+                    ? $url
+                    : null;
+            })
+            ->writeToFile(public_path('sitemap.xml'));
+    }
+}
