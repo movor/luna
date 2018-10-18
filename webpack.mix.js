3161423e9702fd3737f255822987dc5963035fd9
Vladimir Ković <vlada.kovic@gmail.com>
2018-10-18 12:50:13 +0200

    Rename assets folder sass to scss TR-Sy0djiBc

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 let mix = require('laravel-mix');
+mix.disableNotifications();
 
 /*
  |--------------------------------------------------------------------------
@@ -15,23 +16,23 @@ let mix = require('laravel-mix');
 // Used only for watching, to speed up building process
 if (process.env.NODE_ENV === 'local') {
     mix.js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/sass/layout_default.scss', 'public/css')
-        .sass('resources/assets/sass/layout_error.scss', 'public/css');
+        .sass('resources/assets/scss/layout_default.scss', 'public/css')
+        .sass('resources/assets/scss/layout_error.scss', 'public/css');
 }
 // Development: Compile all assets, non minified, without file versions
 else if (process.env.NODE_ENV === 'development') {
     mix.js('resources/assets/js/vendor.js', 'public/js')
         .js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/sass/vendor.scss', 'public/css')
-        .sass('resources/assets/sass/layout_default.scss', 'public/css')
-        .sass('resources/assets/sass/layout_error.scss', 'public/css');
+        .sass('resources/assets/scss/vendor.scss', 'public/css')
+        .sass('resources/assets/scss/layout_default.scss', 'public/css')
+        .sass('resources/assets/scss/layout_error.scss', 'public/css');
 }
 // Production: compile all assets with versions, minified
 if (process.env.NODE_ENV == 'production') {
     mix.js('resources/assets/js/vendor.js', 'public/js')
         .js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/sass/vendor.scss', 'public/css')
-        .sass('resources/assets/sass/layout_default.scss', 'public/css')
-        .sass('resources/assets/sass/layout_error.scss', 'public/css')
+        .sass('resources/assets/scss/vendor.scss', 'public/css')
+        .sass('resources/assets/scss/layout_default.scss', 'public/css')
+        .sass('resources/assets/scss/layout_error.scss', 'public/css')
         .version();
 }
