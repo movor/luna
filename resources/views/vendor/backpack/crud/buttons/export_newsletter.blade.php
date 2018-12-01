017b3821e8f2009a4263df2fc12d1f1077a8911e
Vladimir Ković <vlada.kovic@gmail.com>
2018-12-01 21:56:58 +0100

    Remove old Backpack version TR-QptNUaFT

--- resources/views/vendor/backpack/crud/buttons/export_newsletter.blade.php
+++
@@ -1,9 +0,0 @@
-@if ($crud->hasAccess('create'))
-
-    <a href="{{ url($crud->route . '/export') }}" class="btn btn-primary ladda-button" data-style="zoom-in">
-        <span class="ladda-label">
-            <i class="fa fa-at"></i> Export emails to CSV
-        </span>
-    </a>
-
-@endif
