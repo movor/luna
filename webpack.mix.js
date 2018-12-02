8bd990c9cfc628db4296a1564c06ef98f8ee535e
Vladimir Ković <vlada.kovic@gmail.com>
2018-12-02 15:49:09 +0100

    Root folder merged TR-QptNUaFT

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,24 +15,24 @@ mix.disableNotifications();
 // Local: compile only app assets (without large vendor ones)
 // Used only for watching, to speed up building process
 if (process.env.NODE_ENV === 'local') {
-    mix.js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/scss/layout_default.scss', 'public/css')
-        .sass('resources/assets/scss/layout_error.scss', 'public/css');
+    mix.js('resources/js/app.js', 'public/js')
+        .sass('resources/scss/layout_default.scss', 'public/css')
+        .sass('resources/scss/layout_error.scss', 'public/css');
 }
 // Development: Compile all assets, non minified, without file versions
 else if (process.env.NODE_ENV === 'development') {
-    mix.js('resources/assets/js/vendor.js', 'public/js')
-        .js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/scss/vendor.scss', 'public/css')
-        .sass('resources/assets/scss/layout_default.scss', 'public/css')
-        .sass('resources/assets/scss/layout_error.scss', 'public/css');
+    mix.js('resources/js/vendor.js', 'public/js')
+        .js('resources/js/app.js', 'public/js')
+        .sass('resources/scss/vendor.scss', 'public/css')
+        .sass('resources/scss/layout_default.scss', 'public/css')
+        .sass('resources/scss/layout_error.scss', 'public/css');
 }
 // Production: compile all assets with versions, minified
 if (process.env.NODE_ENV == 'production') {
-    mix.js('resources/assets/js/vendor.js', 'public/js')
-        .js('resources/assets/js/app.js', 'public/js')
-        .sass('resources/assets/scss/vendor.scss', 'public/css')
-        .sass('resources/assets/scss/layout_default.scss', 'public/css')
-        .sass('resources/assets/scss/layout_error.scss', 'public/css')
+    mix.js('resources/js/vendor.js', 'public/js')
+        .js('resources/js/app.js', 'public/js')
+        .sass('resources/scss/vendor.scss', 'public/css')
+        .sass('resources/scss/layout_default.scss', 'public/css')
+        .sass('resources/scss/layout_error.scss', 'public/css')
         .version();
 }
