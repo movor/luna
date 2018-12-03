46fac466b74fb04dc4d1d6b16a8a2fc5f272ee61
Vladimir Ković <vlada.kovic@gmail.com>
2018-12-03 10:23:07 +0100

    Barryvdh laravel cors kernel prepare TR-rfzxN1ap

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -18,7 +18,8 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\TrustProxies::class,
+        // \App\Http\Middleware\TrustProxies::class,
+        \Movor\LaravelDbRedirector\Http\Middleware\DbRedirectorMiddleware::class,
     ];
 
     /**
@@ -35,11 +36,13 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\ShareMessagesFromSession::class
         ],
 
         'api' => [
             'throttle:60,1',
             'bindings',
+            //\Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
