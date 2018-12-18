7bac8bcdd25c3274e27b6ca4d24ce11d02885067
Vladimir Ković <vlada.kovic@gmail.com>
2018-12-18 15:13:06 +0100

    Our custom error page didn't render correctly TR-nQ570bmH

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\AppExceptions\AppErrorException;
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -29,7 +32,8 @@ class Handler extends ExceptionHandler
     /**
      * Report or log an exception.
      *
-     * @param  \Exception  $exception
+     * @param \Exception $exception
+     *
      * @return void
      */
     public function report(Exception $exception)
@@ -40,12 +44,30 @@ class Handler extends ExceptionHandler
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
+     * @param  \Illuminate\Http\Request $request
+     * @param  Exception                $exception
+     *
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
     {
+        // Handle different type of exceptions here
+        if ($exception instanceof NotFoundHttpException) {
+            return response()->view('errors.error', [
+                'title' => 'Page Not Found',
+                'body' => "We can't find the page you're looking for",
+            ], 404);
+        } elseif ($exception instanceof ModelNotFoundException) {
+            return response()->view('errors.error', [
+                'title' => 'Record Not Found',
+                'body' => "We can't find the record you're looking for",
+            ], 404);
+        } elseif ($exception instanceof AppErrorException) {
+            return response()->view('errors.error', [
+                'body' => $exception->getMessage(),
+            ], $exception->getCode());
+        }
+
         return parent::render($request, $exception);
     }
 }
--- resources/views/errors/error.blade.php
+++ resources/views/errors/error.blade.php
@@ -2,14 +2,15 @@
 
 @section('content')
 
+
     <div class="container">
         <div class="text-center my-5 py-5">
             <div class="top mb-5">
                 <span style="font-size: 200px" class="fa fa-frown-o text-primary"></span>
             </div>
             <div class="middle mb-5">
-                <h1>{{ $title or 'Ooops! Something went wrong' }}</h1>
-                <p class="lead">{{ $body or 'Hold tight, we are already working on it' }}</p>
+                <h1>{{ $title ?? 'Ooops! Something went wrong' }}</h1>
+                <p class="lead">{{ $body ?? 'Hold tight, we are already working on it' }}</p>
             </div>
             <div class="bottom">
                 <a href="/" class="btn btn-lg btn-outline-primary">Back Home</a>
